97cf16f0231030943ed35efa31db42daa0293c7d
mustaeen <136145181+must108@users.noreply.github.com>
2024-05-03 12:33:37 -0400

    moved to windows

--- musicnotes/sound.py
+++ musicnotes/sound.py
@@ -209,9 +209,8 @@ elif system == 'Darwin':
             from AppKit import NSSound
         except ImportError:
             log.warning("this library is current running on a python 2 subprocess. run 'pip install PyObjC' for better results.")
-            raise Exception("python 2 is currently not supported!")
-            # soundPlayer = lambda sound, block = True: anotherPython('/System/Library/Frameworks/Python.framework/Versions/2.7/bin/python', 
-            #                                                       sound, block, macOS = True)
+            soundPlayer = lambda sound, block = True: anotherPython('/System/Library/Frameworks/Python.framework/Versions/2.7/bin/python', 
+                                                                  sound, block, macOS = True)
 else:
     soundPlayer = nixSound
     if __name__ != '__main__': # prevent infinite recursion
@@ -219,8 +218,7 @@ else:
             gi.require_version('Gst', '1.0')
         except:
             log.warning("this library is running on another python subprocess. run 'pip install pygobject for better results.")
-            raise Exception("python 2 is currently not supported!")
-            # soundPlayer = lambda sound, block = True: anotherPython('/usr/bin/python3', sound, block, macOS = False)
+            soundPlayer = lambda sound, block = True: anotherPython('/usr/bin/python3', sound, block, macOS = False)
 
 del system
 
